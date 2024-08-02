b2766158f508b174696d75fc499b4abfeea94e8a
lightdevx <khan.m.hasanuzzaman@gmail.com>
2024-08-02 13:24:55 +0600

    u

--- components/Header.jsx
+++ components/Header.jsx
@@ -21,16 +21,16 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
 
-const products = [
-    { name: 'Analytics', href: '#', },
-    { name: 'Engagement', href: '#', },
-    { name: 'Security', href: '#', },
-    { name: 'Integrations', href: '#', },
-    { name: 'Automations', href: '#', },
-]
-const callsToAction = [
-    { name: 'Watch demo', href: '#', },
-    { name: 'Contact sales', href: '#', },
+const services = [
+    { name: 'Clipping Path Services', href: '#', },
+    { name: 'Color Variants & Color Correction', href: '#', },
+    { name: 'Drop Shadow Services', href: '#', },
+    { name: 'Ghost Mannequin Services', href: '#', },
+    { name: 'Image Masking Services', href: '#', },
+    { name: 'Jewelry Photo Editing Services', href: '#', },
+    { name: 'Photo Retouching Services', href: '#', },
+    { name: 'Product Photo-editing Services', href: '#', },
+    { name: 'Real Estate Photo Editing Services(HDR)', href: '#', },
 ]
 
 
@@ -38,7 +38,7 @@ const Header = () => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-        <header className="bg-white">
+        <header className="bg-slate-400">
             <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
                 <div className="flex lg:flex-1">
                     <Link href="#" className="-m-1.5 p-1.5">
@@ -54,31 +54,34 @@ const Header = () => {
                     <button
                         type="button"
                         onClick={() => setMobileMenuOpen(true)}
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
                     >
                         <span className="sr-only">Open main menu</span>
                         <Bars3Icon aria-hidden="true" className="h-6 w-6" />
                     </button>
                 </div>
                 <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+                    <Link href="#" className="text-sm font-semibold leading-6 text-white">
+                        Home
+                    </Link>
                     <Popover className="relative">
-                        <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-                            Product
-                            <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none text-gray-400" />
+                        <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-white">
+                            Services
+                            <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none text-white" />
                         </PopoverButton>
 
                         <PopoverPanel
                             transition
-                            className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
+                            className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-slate-400 shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
                         >
                             <div className="p-4">
-                                {products.map((item) => (
+                                {services.map((item) => (
                                     <div
                                         key={item.name}
-                                        className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                                        className="group relative flex items-center gap-x-6 rounded-lg p-4 text-lg leading-6 hover:bg-indigo-400"
                                     >
                                         <div className="flex-auto">
-                                            <Link href={item.href} className="block font-semibold text-gray-900">
+                                            <Link href={item.href} className="block font-semibold text-white">
                                                 {item.name}
                                                 <span className="absolute inset-0" />
                                             </Link>
@@ -87,31 +90,21 @@ const Header = () => {
                                     </div>
                                 ))}
                             </div>
-                            <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                                {callsToAction.map((item) => (
-                                    <Link
-                                        key={item.name}
-                                        href={item.href}
-                                        className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
-                                    >
-                                    </Link>
-                                ))}
-                            </div>
                         </PopoverPanel>
                     </Popover>
 
-                    <Link href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Features
+                    <Link href="#" className="text-sm font-semibold leading-6 text-white">
+                        Pricing
                     </Link>
-                    <Link href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Marketplace
+                    <Link href="#" className="text-sm font-semibold leading-6 text-white">
+                        Get Quote
                     </Link>
-                    <Link href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Company
+                    <Link href="#" className="text-sm font-semibold leading-6 text-white">
+                        Contact
                     </Link>
                 </PopoverGroup>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <Link href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                    <Link href="#" className="text-sm font-semibold leading-6 text-white">
                         Log in <span aria-hidden="true">&rarr;</span>
                     </Link>
                 </div>
@@ -141,17 +134,17 @@ const Header = () => {
                         <div className="-my-6 divide-y divide-gray-500/10">
                             <div className="space-y-2 py-6">
                                 <Disclosure as="div" className="-mx-3">
-                                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-white hover:bg-gray-50">
                                         Product
                                         <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none group-data-[open]:rotate-180" />
                                     </DisclosureButton>
                                     <DisclosurePanel className="mt-2 space-y-2">
-                                        {[...products, ...callsToAction].map((item) => (
+                                        {services.map((item) => (
                                             <DisclosureButton
                                                 key={item.name}
-                                                as="a"
+                                                as="Link"
                                                 href={item.href}
-                                                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-white hover:bg-gray-50"
                                             >
                                                 {item.name}
                                             </DisclosureButton>
@@ -160,19 +153,19 @@ const Header = () => {
                                 </Disclosure>
                                 <Link
                                     href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
                                 >
                                     Features
                                 </Link>
                                 <Link
                                     href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
                                 >
                                     Marketplace
                                 </Link>
                                 <Link
                                     href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
                                 >
                                     Company
                                 </Link>
@@ -180,7 +173,7 @@ const Header = () => {
                             <div className="py-6">
                                 <Link
                                     href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-50"
                                 >
                                     Log in
                                 </Link>
